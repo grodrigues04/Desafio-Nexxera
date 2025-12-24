84334f5a56e9f9850b67007d902afdc0842dd983
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2025-12-24 02:38:38 -0300

    feat: add useful constants

---
+++ src/constants.py
@@ -0,0 +1,60 @@
+POINTS_LINES= "POINTS_LINES"
+STATES = "STATES"
+DATE = "DATE"
+PAYMENT_AMOUNT = "PAYMENT_AMOUNT"
+ORDENATION = {
+    "DESCENDING":"DESCENDING", 
+    "ASCENDING":"ASCENDING"
+}
+
+COLUMN_FORMAT_RULES = {
+    "NUMERO_INCRICAO_EMPRESA": POINTS_LINES,
+    "DATA_PAGAMENTO": DATE,
+    "ESTADO": STATES,
+    "VALOR_PAGAMENTO": PAYMENT_AMOUNT
+}
+
+brazilian_states = {
+    "AC": "Acre",
+    "AL": "Alagoas",
+    "AP": "Amapá",
+    "AM": "Amazonas",
+    "BA": "Bahia",
+    "CE": "Ceará",
+    "DF": "Distrito Federal",
+    "ES": "Espírito Santo",
+    "GO": "Goiás",
+    "MA": "Maranhão",
+    "MT": "Mato Grosso",
+    "MS": "Mato Grosso do Sul",
+    "MG": "Minas Gerais",
+    "PA": "Pará",
+    "PB": "Paraíba",
+    "PR": "Paraná",
+    "PE": "Pernambuco",
+    "PI": "Piauí",
+    "RJ": "Rio de Janeiro",
+    "RN": "Rio Grande do Norte",
+    "RS": "Rio Grande do Sul",
+    "RO": "Rondônia",
+    "RR": "Roraima",
+    "SC": "Santa Catarina",
+    "SP": "São Paulo",
+    "SE": "Sergipe",
+    "TO": "Tocantins"
+}
+
+mouths = {
+    1: "Janeiro",
+    2: "Fevereiro",
+    3: "Marco",
+    4: "Abril",
+    5: "Maio",
+    6: "Junho",
+    7: "Julho",
+    8: "Agosto",
+    9: "Setembro",
+    10: "Outubro",
+    11: "Novembro",
+    12: "Dezembro"
+}
