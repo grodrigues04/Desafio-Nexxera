cbe198e153548174fc0b57d6d724621ec7e59639
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2025-12-26 17:05:47 -0300

    feat: add a parameter when a specific name is used

--- src/converter.py
+++ src/converter.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
-def converter(df):
+def converter(df, output_name="output"):
     print("Converting dataframe to CSV...")
+    output_path = f"data/{output_name}.csv"
     df.to_csv(
-        "data/output.csv",
+        output_path,
         index=False,        
         sep=";",             
         encoding="utf-8-sig" # evita problema de acentuação no Excel
     )
-    print("Conversion complete. File saved as data/output.csv")
+    print(f"Conversion complete. File saved as {output_path}")
 if __name__ == "__main__":
     converter()
