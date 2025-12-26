995d36d9f35ae89b2ea758bc3ba69f8e0884467a
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2025-12-26 17:03:04 -0300

    refactor: use of list comprehension and add functions to specific formatting

--- src/formater.py
+++ src/formater.py
@@ -1,10 +1,22 @@
 from src import constants
 from datetime import datetime
 
+def position_comma(value):
+    value = str(value)
+    value = value.replace(",", "")
+    comma_position = len(value) - 2  # define first comma position
+    while comma_position > 0:
+        value = value[:comma_position] + "," + value[comma_position:]
+        comma_position -= 3  # jump three positions back
+    return value
+
+def timestamp_to_string(ts: datetime) -> str:
+    return f"{ts.day:02d}/{constants.MONTHS_MAP[ts.month]}/{ts.year}"
+
+
 def formater(rule, data, column=None, extraRule=None):
 
     match rule:
-    
         case constants.POINTS_LINES:
             new_column = []
             for value in data:
@@ -12,30 +24,13 @@ def formater(rule, data, column=None, extraRule=None):
             return new_column
 
         case constants.STATES:
-            new_column = []
-            for value in data:
-                new_column.append(constants.brazilian_states[value])
-            return new_column
+            return [constants.BRAZILIAN_STATES_MAP[value] for value in data]
 
         case constants.DATE:
-            new_column = []
-            for value in data:
-                day = value.day
-                month_name = constants.mouths[value.month]
-                year = value.year
-                year = int(f"{year:02d}")
-                new_column.append(f"{day:02d}/{month_name}/{year}")
-            return new_column
+            return [timestamp_to_string(value) for value in data]
 
         case constants.PAYMENT_AMOUNT:
-            new_column = []
-            for value in data:
-                value = str(value)
-                if "," not in value:
-                    value = f"{value},00"
-                value = f"R${value}"
-                new_column.append(value)        
-            return new_column
+            return [position_comma(value) for value in data]
 
 
 if __name__ == "__main__":
